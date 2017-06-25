ab7dfb1eb431429760ee92f0471c4acdbdd74694
datasalad <sorokolat.s@gmail.com>
2017-06-25 16:54:02 +0300

    updated plot 3 and 4

--- plot3.R
+++ plot3.R
@@ -3,8 +3,8 @@ constructPlot3 <- function(dataset) {
     with(dataset, 
          {
             plot(Sub_metering_1 ~ DateConverted, type = "l",  ylab = "Energy sub metering", xlab = "")
-            lines(Sub_metering_2 ~ DateConverted, col = 'red')
-            lines(Sub_metering_3 ~ DateConverted, col = 'blue')
+            lines(Sub_metering_2 ~ DateConverted, col = "red")
+            lines(Sub_metering_3 ~ DateConverted, col = "blue")
         })
     legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     dev.off()
